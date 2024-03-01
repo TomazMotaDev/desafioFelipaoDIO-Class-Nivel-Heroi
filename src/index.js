c6f0f8d5e7af6a6e2793ff38899f5be141109a37
Tomaz Mota <tomazfmota@gmail.com>
2024-02-29 21:02:38 -0300

    commit do arquivo index.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+let nomeHeroi = "Juvenal"
+let exp = 2574
+
+if (exp <= 1000){
+    console.log("O Herói de nome "+nomeHeroi+" está no nível de Ferro")
+}else if (exp > 1000 && exp <= 2000) {
+    console.log("O Herói de nome "+nomeHeroi+" está no nível de Bronze")
+}else if (exp > 2000 && exp <= 2000) {
+    console.log("O Herói de nome "+nomeHeroi+" está no nível de Prata")
+}else if (exp > 5000 && exp <= 5000) {
+    console.log("O Herói de nome "+nomeHeroi+" está no nível de Ouro")
+}else if (exp > 7000 && exp <= 7000) {
+    console.log("O Herói de nome "+nomeHeroi+" está no nível de Platina")
+}else if (exp > 8000 && exp <= 8000) {
+    console.log("O Herói de nome "+nomeHeroi+" está no nível de Ascendente")
+}else if (exp > 9000 && exp <= 10000) {
+    console.log("O Herói de nome "+nomeHeroi+" está no nível de Imortal")
+}else if (exp > 10000) {
+    console.log("O Herói de nome "+nomeHeroi+" está no nível de Radiante")
+}
